1e1414c362f3c90243a8e38a7e4f5689888f4f99
BrayanMonsan <99520812+BrayanMonsan@users.noreply.github.com>
2023-03-14 18:47:47 -0600

    Add files via upload

---
+++ Demo1/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        h1{
+            color: red;
+        }
+    </style>
+    <title>Demo 1</title>
+</head>
+<body>
+    <!--Comentario en hrml-->
+    <p>Hola mundo desde HTML</p>
+    <?php echo 'Hola mundo desde PHP';
+    echo '<h1>Programación Web</h1>';
+    echo '<hr>';
+    //comentario 1 linea
+    /*comentario 
+    multilineas*/
+    echo '<p>Unach</p>';
+    echo '<p>5° "J"</p>';
+    echo '<a href="perfil.php">Mi perfil de html</a>'
+    ?>
+    <a href="perfil.php">Mi perfil de html</a>
+</body>
+</html>
---
+++ Demo2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo2</title>
+</head>
+<body>
+    <?php
+    $nombre='Brayan';
+    $edad='21';
+    $sueldo='3500.50';
+    echo $nombre;
+    echo 'Tu edad es:' .$edad. '<br>';
+    echo 'Tu sueldo Neto es:' .$sueldo. '<br>';
+    $liquido=$sueldo-1000;
+    echo 'Tu sueldo liquido es:' .$liquido;
+    ?>
+</body>
+</html>
